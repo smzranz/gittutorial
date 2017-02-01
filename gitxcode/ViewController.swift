7b2201399d20c9710c3b26df147846ba4c6a4737
Shamshir Anees <shamshir@citruss-mac-mini-2.local>
2017-02-01 11:36:21 +0530

    Initial Commit

---
+++ gitxcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitxcode
+//
+//  Created by Shamshir Anees on 01/02/17.
+//  Copyright © 2017 Shamshir Anees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
